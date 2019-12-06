1f5bffc14fb1bc7cf4e2aaff91a4091825391b87
jacoelho <jose.alberto.coelho@gmail.com>
2019-12-06 12:27:40 +0000

    part2

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque};
 use std::fs;
 
 fn main() -> Result<(), ::std::io::Error> {
@@ -9,28 +9,54 @@ fn main() -> Result<(), ::std::io::Error> {
         .map(|c| c.split(")").collect::<Vec<_>>());
 
     let orbits = orbit_map.fold(HashMap::new(), |mut state, route| {
-        let child = state.entry(route[0].to_string()).or_insert(Vec::new());
-        child.push(route[1].to_string());
+        (*state.entry(route[0].to_string()).or_insert(Vec::new())).push(route[1].to_string());
+        (*state.entry(route[1].to_string()).or_insert(Vec::new())).push(route[0].to_string());
 
         state
     });
 
-    println!("{:?}", part_1("COM".to_string(), 0, &orbits));
+    println!("{:?}", part1(&orbits));
+
+    println!("{:?}", part2(&orbits));
 
     Ok(())
 }
 
-fn part_1(orbit: String, weight: usize, h: &HashMap<String, Vec<String>>) -> usize {
-    match h.get(&orbit) {
-        None => weight,
-        Some(v) => {
-            let mut sum = weight;
+fn part1(orbits: &HashMap<String, Vec<String>>) -> usize {
+    traverse_orbits("COM", &orbits).values().sum::<usize>()
+}
 
-            for child in v.iter() {
-                sum += part_1(child.to_string(), weight + 1, h)
+fn part2(orbits: &HashMap<String, Vec<String>>) -> Option<usize> {
+    traverse_orbits("YOU", &orbits).get("SAN").copied()
+}
+
+fn traverse_orbits<'a>(
+    start: &'a str,
+    orbits: &'a HashMap<String, Vec<String>>,
+) -> HashMap<&'a str, usize> {
+    let mut to_visit = VecDeque::new();
+    let mut visited: HashMap<&str, usize> = HashMap::new();
+
+    to_visit.push_back((start, 0));
+
+    while !to_visit.is_empty() {
+        let (orbit, distance) = match to_visit.pop_back() {
+            Some((a, b)) => (a, b),
+            None => break,
+        };
+
+        if visited.contains_key(orbit) {
+            continue;
+        }
+
+        visited.insert(orbit, distance);
+
+        if let Some(children) = orbits.get(orbit) {
+            for child in children {
+                to_visit.push_back((child, distance + 1));
             }
-
-            sum
         }
     }
+
+    visited
 }
