490765f06806f2656ce0e8e926dacac65bab0b46
jacoelho <jose.alberto.coelho@gmail.com>
2020-12-05 11:53:38 +0000

    Day05

---
+++ 2020/Day05.fs
@@ -0,0 +1,69 @@
+module Day05
+
+open System.IO
+
+module Direction =
+    type T =
+        | F
+        | B
+        | L
+        | R
+
+    let private fromChar ch =
+        match ch with
+        | 'F' -> Some F
+        | 'B' -> Some B
+        | 'L' -> Some L
+        | 'R' -> Some R
+        | _ -> None
+
+    let fromString (str: string) =
+        str.ToCharArray() |> Array.choose fromChar
+
+let findSeat (seats: int seq) =
+    Seq.fold (fun seats code ->
+        let len = seats |> Seq.length
+
+        match code with
+        | Direction.F -> seats |> Seq.take (len >>> 1)
+        | Direction.B -> seats |> Seq.skip (len >>> 1)
+        | Direction.L -> seats |> Seq.take (len >>> 1)
+        | Direction.R -> seats |> Seq.skip (len >>> 1)) seats
+
+let row seat =
+    seat
+    |> Array.toSeq
+    |> Seq.take 7
+    |> findSeat (seq { 0 .. 127 })
+    |> Seq.head
+
+let column seat =
+    seat
+    |> Array.toSeq
+    |> Seq.skip 7
+    |> findSeat (seq { 0 .. 7 })
+    |> Seq.head
+
+let seatID str =
+    let directions = str |> Direction.fromString
+
+    8 * (directions |> row) + (directions |> column)
+
+
+let readInput file = file |> File.ReadAllLines
+
+let Part1 file =
+    file |> readInput |> Array.map seatID |> Array.max
+
+let Part2 file =
+    let seats = file |> readInput |> Array.map seatID
+
+    let allSeats =
+        seq { (seats |> Array.min) .. (seats |> Array.max) }
+        |> Set.ofSeq
+
+    let seatsSet = seats |> Set.ofArray
+
+    Set.difference allSeats seatsSet
+    |> Set.toList
+    |> List.head
--- 2020/Program.fs
+++ 2020/Program.fs
@@ -1,8 +1,8 @@
-open Day04
+open Day05
 
 [<EntryPoint>]
 let main argv =
-    let input = "inputs/day04.txt"
-    printfn "Part01: %A" ( input |> Day04.Part1)
-    //printfn "Part02: %A" (input |> Day03.Part2)
+    let input = "inputs/day05.txt"
+    printfn "Part01: %A" ( input |> Day05.Part1)
+    printfn "Part02: %A" (input |> Day05.Part2)
     0
