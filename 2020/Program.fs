46f045385433394e9c236b692fae21f5a1c6dc8b
jacoelho <jose.alberto.coelho@gmail.com>
2020-12-12 14:35:51 +0000

    day12

--- 2020/Program.fs
+++ 2020/Program.fs
@@ -1,10 +1,10 @@
-open Day11
+open Day12
 open System.IO
 
 let readInput file = file |> File.ReadAllLines
 
 [<EntryPoint>]
 let main argv =
-    printfn "Part01: %A" ( argv.[0] |> readInput |> Day11.Part1)
-    printfn "Part02: %A" ( argv.[0] |> readInput |> Day11.Part2)
+    printfn "Part01: %A" ( argv.[0] |> readInput |> Day12.Part1)
+    printfn "Part02: %A" ( argv.[0] |> readInput |> Day12.Part2)
     0
