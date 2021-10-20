e523542ec7e870822ed58ce508acfb1617156500
Jose Coelho <jose.alberto.coelho@gmail.com>
2021-10-20 23:01:59 +0100

    day 10

--- clojure/advent-of-code/src/jacoelho/aoc.clj
+++ clojure/advent-of-code/src/jacoelho/aoc.clj
@@ -23,3 +23,13 @@
 
 (defn ->long [l]
   (Long/parseLong l))
+
+(defn long->digits [^long num]
+  (loop [n num
+         res '()]
+    (if (= n 0)
+      res
+      (recur (quot n 10) (conj res (rem n 10))))))
+
+(defn digits->long [col]
+  (reduce #(+ (* 10 %) %2) (long 0) col))
